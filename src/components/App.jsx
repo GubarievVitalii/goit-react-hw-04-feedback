b705bdea650d25a30cb60cd8b1a4ce380f756415
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-11-08 01:08:29 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,8 @@ export default function App() {
       case 'bad':
         setBad(bad + 1);
         break;
-      default: console.log(feedbackState)
+      default:
+        console.log(feedbackState);
     }
   };
 
@@ -36,10 +37,7 @@ export default function App() {
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={feedbackState}
-        />
+        <FeedbackOptions options={options} onLeaveFeedback={feedbackState} />
       </Section>
       <Section title="Statistics">
         {total > 0 ? (
@@ -57,54 +55,3 @@ export default function App() {
     </>
   );
 }
-
-// class App extends Component {
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0,
-// };
-
-// feedbackState = name => this.setState(prev => ({ [name]: prev[name] + 1 }));
-
-// countTotalFeedback = () =>
-//   this.state.good + this.state.neutral + this.state.bad;
-
-// countPositiveFeedbackPercentage = () =>
-//   Math.round((this.state.good / this.countTotalFeedback()) * 100);
-
-// render() {
-//   const { good, neutral, bad } = this.state;
-// const total = this.countTotalFeedback();
-// const positiveFeedback = this.countPositiveFeedbackPercentage();
-// const options = ['Good', 'Neutral', 'Bad'];
-
-// return (
-//   <>
-//     <Section title="Please leave feedback">
-//       <FeedbackOptions
-//         options={options}
-//         onLeaveFeedback={this.feedbackState}
-//       />
-//     </Section>
-//     <Section title="Statistics">
-//       {total > 0
-//         ?
-//         <Statistics
-//           good={good}
-//           neutral={neutral}
-//           bad={bad}
-//           total={total}
-//           positiveFeedback={positiveFeedback}
-
-//         />
-//         : <Notification message="There is no feedback"></Notification>
-//       }
-//     </Section>
-//   </>
-
-//     );
-//   }
-// }
-
-// export default App;
