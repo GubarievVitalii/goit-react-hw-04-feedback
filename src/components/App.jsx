e87bae7ebebb2a23f7e0e0e0de2f50ec96b2b73c
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-11-08 01:15:30 +0200

    +++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import React from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
